04f27cf18152eb51b028d64ef15d819a15eec12d
harshit likhar <harshitlikhar007@gmail.com>
2019-06-25 23:51:44 +0530

    Add files via upload

---
+++ includes/widget.php
@@ -0,0 +1,4 @@
+<div class="well">
+	<h4>Side Widget Well</h4>
+		<p>A blog is an online diary or journal located on a website. The content of a blog typically includes text, pictures, videos, animated GIFs and even scans from old physical offline diaries or journals and other hard copy documents.</p>
+</div>
